24892aa4d4aac37eb824e48690f31b32a5e4abde
LEAH_HAMILTON <leah.h.b.hamilton@gmail.com>
2024-01-19 17:24:46 -0800

    added fetch, need to update node package fully probably

--- program.js
+++ program.js
@@ -1,4 +1,15 @@
-import Axios from 'axios';
+//import Axios from 'axios';
 
+//console.log(Axios)
 
-base_url = 'http://www.irs.gov/uac/Authorized-IRS-e-file-Providers-for-Individuals'
+let base_url =
+  "http://www.irs.gov/uac/Authorized-IRS-e-file-Providers-for-Individuals";
+
+//const resp = Axios.get(base_url)
+
+async function scrapeIRS() {
+    const response = await fetch(base_url)
+    console.log(response)
+}
+
+scrapeIRS();
