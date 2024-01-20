1d4d152a86cb2676d893c44a4e71a711c2d66da8
LEAH_HAMILTON <leah.h.b.hamilton@gmail.com>
2024-01-19 20:10:12 -0800

    by george we've done it

--- program.js
+++ program.js
@@ -1,33 +1,48 @@
-//import Axios from 'axios';
-import fetch from "node-fetch";
 
-//console.log(Axios)
+import fetch from "node-fetch";
 
 let baseUrl =
   "http://www.irs.gov/uac/Authorized-IRS-e-file-Providers-for-Individuals";
 
-//const resp = Axios.get(base_url)
-
 async function scrapeIRS() {
-    const response = await fetch(base_url);
-    const data = await response.text()
-    console.log(data)
+  const response = await fetch(baseUrl);
+  const data = await response.text();
+  console.log(data);
 }
 
-// This provides us with all of the HTML scraped off the page 
+// This provides us with all of the HTML scraped off the page
 //scrapeIRS();
 
-// Change for desired zipcode, had to use Malibu as the example of course
-const zipcode = 90265;
-
+// Now to search by zipcode
 async function searchByZip(zip) {
-    const zipUrl = `https://www.irs.gov/efile-index-taxpayer-search?zip=${zip}&state=All`
-    const response = await fetch(zipUrl);
-    const data = await response.text()
-    console.log(data)
+  const zipUrl = `https://www.irs.gov/efile-index-taxpayer-search?zip=${zip}&state=All`;
+  const response = await fetch(zipUrl);
+  const data = await response.text();
+  console.log(data);
 }
 
-searchByZip(zipcode);
+// Change for desired zipcode, had to use Malibu as the example of course
+// If zip is left blank it searches all IRS e-file Providers
+const zipcode = 90265;
 
+//searchByZip(zipcode);
 
 //Now add parameters for name and phone number to search
+// need to filter text from response...
+
+async function searchByParams(zip, name, phone) {
+  const zipUrl = `https://www.irs.gov/efile-index-taxpayer-search?zip=${zip}&state=All`;
+  const response = await fetch(zipUrl);
+  const data = await response.text();
+  //console.log(data);
+  const dataArray = data.split("<tr>")
+  //console.log(dataArray)
+  let filtered = dataArray.filter((ind) => ind.includes(name || phone))
+  console.log(filtered)
+  return filtered
+}
+
+searchByParams(90265, "BONNIE JO ROSS","(310)453-1145")
+
+//convert phone to string? strict format?
+//idea to parse is to turn entire html into arrays split at < and >? then map or filter for <tr>, create a new array of the tr tags and then filter those for the name/number, maybe use ||?
