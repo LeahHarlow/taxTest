6f3dcc72103eefc6dbb8440dbb957ca8b0124581
LEAH_HAMILTON <leah.h.b.hamilton@gmail.com>
2024-01-19 21:09:34 -0800

    fixed typo

--- program.js
+++ program.js
@@ -37,14 +37,14 @@ async function searchByParams(zip, name, phone) {
   // Split text of HTML into an array to give us Array methods for filtration based on the input params.
   // Splitting at <tr> gives the HTML for each individual Provider like a business card.
   const dataArray = data.split("<tr>")
-  // Now run a filter over the array of Providors to search for name or number
+  // Now run a filter over the array of Providers to search for name or number
   let filtered = dataArray.filter((index) => index.includes(name || phone))
   console.log(filtered)
   return filtered
 }
 
 
-// Basic tests without intigrating a testing framework, resolution of promises from async functions may have them in a different order.
+// Basic tests without integrating a testing framework, resolution of promises from async functions may have them in a different order.
 console.log("Expected HTML for BONNIE JO ROSS and got",(searchByParams(90265, "BONNIE JO ROSS"))) // Should Pass
 console.log("Expected HTML for ACCOUNTING RIVERS and got",(searchByParams(93010, "ACCOUNTING RIVERS", "(805)335-2460"))) // Should Pass
 console.log("Expected HTML for ANGELICA CHAVEZ and got",(searchByParams(93030, "ANGELICA CHAVEZ","(805)485-6547"))) // Should Pass
@@ -56,4 +56,4 @@ to keep improper capitalization from turning up results. I would also add simila
 input on the front end, converting the phone input into a string etc.
 I would also add error handling to my Async functions to aid in UI*/
 
-//I hope you like my assessment! Thank you for your concideration I look forward to the chance to get to join the team at fileyourtaxes.com.
+//I hope you like my assessment! Thank you for your consideration I look forward to the chance to get to join the team at fileyourtaxes.com.
